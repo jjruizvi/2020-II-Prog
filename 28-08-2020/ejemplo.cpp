a8095d3a433ae72b8e170a6dd5d08746c31336cb
Juan Jose Ruiz Villamil <jjruizvi@unal.edu.co>
2020-08-26 16:28:05 -0500

    se agrega una salida del sin(pi/2)

--- 28-08-2020/ejemplo.cpp
+++ 28-08-2020/ejemplo.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <cmath>
 
 int main(void)
 {
   std::cout<< "Hello World!\n";
-  std::cout<< "Hola Mundo!\n"
+  std::cout<< "Hola Mundo!\n";
+  std::cout<< "sin(pi/2):"<<std::sin(M_PI/2)<<"\n";
+  
   return 0;
 }
